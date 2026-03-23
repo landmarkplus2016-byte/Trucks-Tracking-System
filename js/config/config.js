3d297ad69d2c3a89d65f18308a623650b04057e1
Khaled El-Sherbiny <k.sherbiny@gmail.com>
2026-03-23 06:11:24 +0200

    script fix-2

--- js/config/config.js
+++ js/config/config.js
@@ -1,5 +1,5 @@
 const CONFIG = Object.freeze({
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzB-m5et53xdYwOjKQ71w87tSbAOnZJ09xL01hKMYmuKqWaB-csaBBByaSPijjgeeTS9Q/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwehWt6IZc7bhUW4NqYEzshfY7JEmOEZEB1WSRpwb75yrwqv-Oak5Klnx7XlF8q3Nkl/exec',
   APP_NAME: 'Trucks Tracking System',
   NOTIF_POLL_INTERVAL: 60000,
   SESSION_KEY: 'tts_user'
